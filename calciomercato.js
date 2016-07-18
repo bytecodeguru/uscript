a3742aab457233120962f40789906e219538a2e8
Giuseppe Moscarella <giuseppe@catapush.com>
2016-07-18 11:21:01 +0200

    Remove annoying anti adblock content from calciomercato.com

---
+++ calciomercato.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name         Calciomercato adblock
+// @namespace    http://bytecode.guru/
+// @version      1.2
+// @description  Remove annoying anti adblock content
+// @author       https://github.com/bytecodeguru
+// @updateUrl    https://github.com/bytecodeguru/uscript/blob/master/calciomercato.js
+// @include      http://www.calciomercato.com/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    for (i = 1; i <= 9; i++) {
+        var blockingElement = document.querySelector('[id^="' + i + '"]');
+        if (blockingElement !== null) {
+            document.getElementById(blockingElement.id).style.display = 'none';
+        }
+    }
+    $("div:contains('plugin')").hide();
+})();
