42e6be913905e0f4f381056e15f6e64f7c71b392
Giuseppe Moscarella <bytecodeguru@users.noreply.github.com>
2018-04-17 22:13:18 +0200

    run calciomercato every 1 second

--- calciomercato.js
+++ calciomercato.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Calciomercato adblock
 // @namespace    http://bytecode.guru/
-// @version      1.2.3
+// @version      1.3.0
 // @description  Remove annoying anti adblock content
 // @author       https://github.com/bytecodeguru
 // @updateUrl    https://raw.githubusercontent.com/bytecodeguru/uscript/master/calciomercato.js
@@ -10,14 +10,16 @@
 // @run-at       document-idle
 // ==/UserScript==
 
-(function() {
-    window.onload = function(e) {
-        for (i = 1; i <= 9; i++) {
-          var blockingElement = document.querySelector('[id^="' + i + '"]');
-          if (blockingElement !== null) {
-            document.getElementById(blockingElement.id).style.display = 'none';
-          }
-        }
-        $("div:contains('plugin')").hide();
+hideBlockingElement() {
+    for (i = 0; i <= 9; i++) {
+      var blockingElement = document.querySelector('[id^="' + i + '"]');
+      if (blockingElement !== null) {
+        document.getElementById(blockingElement.id).style.display = 'none';
+      }
     }
+    $("div:contains('plugin')").hide();
+}
+
+(function() {
+    window.setInterval(hideBlockingElement, 1000);
 })();
